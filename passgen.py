ee920fffde93ccb001669c61dc55891067aa4bf5
Proxser <theproxser@gmail.com>
2019-12-09 16:11:06 +0700

    Update passgen.py & README.md

--- passgen.py
+++ passgen.py
@@ -3,12 +3,6 @@
 import argparse
 import random
 
-parser = argparse.ArgumentParser(description = 'PASSWORD GENERATOR v0.0.1', 
-                                 epilog = 'Use -h for help')
-parser.add_argument('-l', '--length',
-                    type = int, default = 8, 
-                    help = 'Enter a password length of at least 4 characters (default: 8)')
-
 default = {
     'numbers': True,
     'uppercase_letter': True,
@@ -34,9 +28,55 @@ def getPassword(length = 8, options = default):
         password.append(random.choice(sym_list))
     return ''.join(password)
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(
+        description = 'PASSWORD GENERATOR v0.0.1', 
+        epilog = 'Use -h for help'
+    )
+    parser.add_argument(
+        '-l', '--length',
+        type = int,
+        default = 8,
+        help = 'Enter a password length of at least 4 characters (default: 8)'
+    )
+    parser.add_argument(
+        '-n', '--numbers',
+        type = bool,
+        default = False,
+        help = 'Use numbers in password (default: False)'
+    )
+    parser.add_argument(
+        '-up', '--uppercase',
+        type = bool,
+        default = False,
+        help = 'Use uppercase letter in password (default: False)'
+    )
+    parser.add_argument(
+        '-lw', '--lowercase',
+        type = bool,
+        default = True,
+        help = 'Use lowercase letter in password (default: True)'
+    )
+    parser.add_argument(
+        '-s', '--special',
+        type = bool,
+        default = False,
+        help = 'Use special chars in password (default: False)'
+    )
+    args = parser.parse_args()
+    print(args)
 
-if args.length <= 3:
-    print('Длина пароля должны быть не менее 4 символов. Попробуйте снова')
-else:
-    print(getPassword(length = args.length))
+    options = {
+        'numbers': args.numbers,
+        'uppercase_letter': args.uppercase,
+        'lowercase_letter': args.lowercase,
+        'special_char': args.special
+    }
+
+    if args.length <= 3:
+        print('Длина пароля должны быть не менее 4 символов. Попробуйте снова')
+    else:
+        print(getPassword(args.length, options))
+
+if __name__ == '__main__':
+    main()
